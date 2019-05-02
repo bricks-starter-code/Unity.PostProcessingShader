8298859d2becaa5d4a3e58a3808088aa41959cf5
B Ricks <bricks@unomaha.edu>
2019-05-02 10:39:52 -0500

    Create post process example

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	//The material with the shader we want to apply as a postprocess
+	public Material mat;
+
+	
+	//See https://docs.unity3d.com/Manual/PostProcessingWritingEffects.html
+	//See https://www.alanzucconi.com/2015/07/08/screen-shaders-and-postprocessing-effects-in-unity3d/
+	//See https://docs.unity3d.com/ScriptReference/Camera.OnRenderImage.html
+	
+	private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		 
+		 //Change the value on the material
+		 mat.SetFloat("_bwBlend", (Mathf.Sin(Time.time)*.5f + .5f));
+		 
+		 //Copy the src to the dest after applying material mat
+		 Graphics.Blit(src, dest, mat);
+	}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Unity.CustomTextureShaders
+Simple example of chaining custom texture shaders in Unity
+
+Running code on the GPU can be significantly faster than on the CPU. This project shows you how to setup a project to do processing on the GPU in Unity using custom texture shaders.
